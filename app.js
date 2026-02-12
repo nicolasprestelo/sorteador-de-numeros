dba263125081fc09bd33e6a8ee01e02f451ca133
nicolasprestelo <nicolasprestelorocha@gmail.com>
2026-02-11 21:04:38 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function sortear(){
+    let quantidade = document.getElementById("quantidade").value;
+    let de = document.getElementById("de").value;
+    let ate = document.getElementById("ate").value;
+
+    alert(`quantidade ${quantidade}`);
+    alert(`de ${de}`);
+    alert(`ate ${ate}`);
+
+
+}
